6e4c2369b220acfe72eba7b19c4e7f13c22bcbf6
Scorlord <113203140+Scorlord@users.noreply.github.com>
2022-09-15 08:19:59 -0700

    Update aircraft_app.py
    IF Else workaround done

--- aircraft_app.py
+++ aircraft_app.py
@@ -7,24 +7,23 @@ factory = AircraftFactory()
 aircraft = "F-80"
 
 aircraft = factory.getAircraft("B-52")  ## Find Specific Aircraft
-#performanceWeight = round((aircraft.maxTakeOffWeight * 1.5),2)  ## desired weight in kilograms
-performanceSpeed = aircraft.maxSpeedSea   ## desired speed in meters per second
-
-def checkWeight(self):
-    self.performanceWeight = aircraft.maxTakeOffWeight
-
-    if self.performanceWeight < aircraft.emptyWeight:
-        print("Weight not acceptable as it is less than the aircraft empty weight.")
-        exit()
-    if self.performanceWeight > aircraft.maxTakeOffWeight:
-        print("Weight not acceptable as it is more than aircraft maxium take off weight.")
-        exit()
-    else:
-        return self.performanceWeight 
-
-    
+performanceWeight = round((aircraft.maxTakeOffWeight * 1),2)  ## desired weight in kilograms
+performanceSpeed = round((aircraft.maxSpeedSea * .5),2)   ## desired speed in meters per second
 
 
+if performanceWeight < aircraft.emptyWeight:
+    print("Weight not acceptable as it is less than the aircraft empty weight.")
+    exit()
+if performanceWeight > aircraft.maxTakeOffWeight:
+    print("Weight not acceptable as it is more than aircraft maxium take off weight.")
+    exit()
+else:
+    pass
+if performanceSpeed < aircraft.minSpeedSea:
+    print("Speed cannot be less than stall speed.")
+    exit()
+else:
+    pass
 
 
 def bestTurnDyanmicPressure(wingArea, thrust, zeroLiftDrag, inducedDrag, maxLift):
@@ -40,7 +39,7 @@ bestTurnSpeedAns = round(bestTurnSpeed(dynamicPressure = bestTurnDyanmicPressure
 def bestTurnLoadFactor(wingArea, maxLift, dynamicPressure, weight):
     return (wingArea * maxLift * 100 * dynamicPressure) / weight
 
-bestTurnFactorAns = bestTurnLoadFactor(wingArea = aircraft.wingArea, maxLift= aircraft.maxLift(), dynamicPressure= bestTurnDyanmicPressureAns, weight = aircraft.performanceWeight)
+bestTurnFactorAns = bestTurnLoadFactor(wingArea = aircraft.wingArea, maxLift= aircraft.maxLift(), dynamicPressure= bestTurnDyanmicPressureAns, weight = performanceWeight)
 
 def bestTurnRate(velocity, loadFactor):
     return (9.8 / velocity) * math.sqrt((loadFactor ** 2) -1) * 180 / math.pi
@@ -55,12 +54,12 @@ bestTurnRadiusAns = round(bestTurnRadius(velocity = bestTurnSpeedAns, loadFactor
 def instantTurnRadiusFunction(weight, pressure,  surfaceArea, maxLift, frameLimit): ## Instant turn radius, typically not equal to substained radius
     return (weight / (0.5 * pressure * surfaceArea * maxLift)) * (frameLimit / math.sqrt((frameLimit **2) - 1))
 
-instantTurnradiusAns = round((instantTurnRadiusFunction(weight = aircraft.performanceWeight, pressure = 1.225, surfaceArea = aircraft.wingArea, maxLift = aircraft.maxLift(), frameLimit = aircraft.structuralLimit)), 2)
+instantTurnradiusAns = round((instantTurnRadiusFunction(weight = performanceWeight, pressure = 1.225, surfaceArea = aircraft.wingArea, maxLift = aircraft.maxLift(), frameLimit = aircraft.structuralLimit)), 2)
 
 def instantTurnRateFunction(weight, pressure,  surfaceArea, maxLift, frameLimit): ## Instant turn rate, typically not equal to substained rate
     return ( ( (9.80665 * (math.sqrt( (0.5 * pressure * surfaceArea * maxLift) / weight)) * (frameLimit / math.sqrt((frameLimit **2) - 1)) ) * 180 ) / math.pi)
 
-instantTurnRateAns = round((instantTurnRateFunction(weight = aircraft.performanceWeight, pressure = 1.225, surfaceArea = aircraft.wingArea, maxLift = aircraft.maxLift(), frameLimit = aircraft.structuralLimit)), 2)
+instantTurnRateAns = round((instantTurnRateFunction(weight = performanceWeight, pressure = 1.225, surfaceArea = aircraft.wingArea, maxLift = aircraft.maxLift(), frameLimit = aircraft.structuralLimit)), 2)
 
 def instantTurnThrustRequired(zeroLiftDrag, inducedDrag, maxLift, velocity, wingArea):
     return ((zeroLiftDrag + inducedDrag * (maxLift**2)) * 1.225 * velocity * wingArea)/1000
@@ -70,28 +69,22 @@ instantTurnThrustRequiredAns = instantTurnThrustRequired(zeroLiftDrag = aircraft
 def performanceWeightPercentage(weight, mtow):
     return (weight / mtow) * 100
 
-weightPercentage = round(aircraft.performanceWeightPercentage(weight = aircraft.performanceWeight, mtow = aircraft.maxTakeOffWeight), 0)
+weightPercentage = round(performanceWeightPercentage(weight = performanceWeight, mtow = aircraft.maxTakeOffWeight), 0)
 
-
-if performanceSpeed < aircraft.minSpeedSea:
-    print("Speed cannot be less than aircraft stall speed, please re-enter.")
-    exit()
-
-if performanceSpeed > aircraft.maxSpeedSea:
-    print("Speed cannot be more than aircraft max sea level speed, please re-enter.")
-    exit()
-
-else:
-    print("The " + aircraft.modelName + " " + aircraft.nickName + " was made in the " + aircraft.nation + " and was introduced in " + str(aircraft.year) + ". The " + aircraft.nickName + " uses a " + aircraft.airFoil + " airfoil, which has a max sea level speed of " + str(aircraft.maxSpeedSea) + " m/s, and stalls at " + str(aircraft.minSpeedSea) + " m/s.")
-    print("With an empty weight of " + str(aircraft.emptyWeight) + " kg, and max-take-off-weight of " + str(aircraft.maxTakeOffWeight) + " kg, which gives it a useful load of " + str(aircraft.maxTakeOffWeight - aircraft.emptyWeight)+ " k/g. Minus the max internal fuel load of " + str(aircraft.maxInternalfuel) + " kg, allows it to carry " + str(aircraft.maxTakeOffWeight - (aircraft.emptyWeight - aircraft.maxInternalfuel)) + " kg mission load.")
-    print("If the " + aircraft.nickName + " is at a speed of " + str(performanceSpeed) + " m/s, and has a weight of " + str(aircraft.performanceWeight) + " kg(" + str(weightPercentage)  + "% of MTOW) we can determine the following.")
+print("The " + aircraft.modelName + " " + aircraft.nickName + " was made in the " + aircraft.nation + " and was introduced in " + str(aircraft.year) + ". The " + aircraft.nickName + " uses a " + aircraft.airFoil + " airfoil, which has a max sea level speed of " + str(aircraft.maxSpeedSea) + " m/s, and stalls at " + str(aircraft.minSpeedSea) + " m/s.")
+print("With an empty weight of " + str(aircraft.emptyWeight) + " kg, and max-take-off-weight of " + str(aircraft.maxTakeOffWeight) + " kg, which gives it a useful load of " + str(aircraft.maxTakeOffWeight - aircraft.emptyWeight)+ " k/g. Minus the max internal fuel load of " + str(aircraft.maxInternalfuel) + " kg, allows it to carry " + str(aircraft.maxTakeOffWeight - (aircraft.emptyWeight - aircraft.maxInternalfuel)) + " kg mission load.")
+print("If the " + aircraft.nickName + " is at a speed of " + str(performanceSpeed) + " m/s, and has a weight of " + str(performanceWeight) + " kg(" + str(weightPercentage)  + "% of MTOW) we can determine the following.")
     
 if instantTurnThrustRequiredAns > (aircraft.thrust + aircraft.afterburner):
-    print("That the " + aircraft.modelName + " has an instant turn rate of " + str(instantTurnRateAns) + " degrees per second, and instant turn radius of " + str(instantTurnradiusAns) + " meters. Which is higher than its substained turn speed of " + str(bestTurnSpeedAns) + " m/s. Allowing it a susbtained turn radius of " + str(bestTurnRadiusAns) + " meters, and turn rate of " + str(bestTurnRateAns) + " degrees per second.")
+    print("That the " + aircraft.modelName + " has an instant turn rate of " + str(instantTurnRateAns) + " degrees per second, and instant turn radius of " + str(instantTurnradiusAns) + " meters, which is higher than its substained turn rate.")
+    print("Where its susbtained turn occurs at " + str(bestTurnSpeedAns) + " m/s, giving it a radius of " + str(bestTurnRadiusAns) + " meters, and turn rate of " + str(bestTurnRateAns) + " degrees per second.")
+    exit()
 
 if instantTurnThrustRequiredAns < (aircraft.thrust + aircraft.afterburner):
-    print("That the " + aircraft.modelName + " has a max instant turn rate of " + str(instantTurnRateAns) + " degrees per second, and substained turn radius of " + str(instantTurnradiusAns) + " meters. Which is lower than its substained turn speed of " + str(bestTurnSpeedAns) + " m/s. Allowing it a substained turn radius of " + str(bestTurnRadiusAns) + " meters, and turn rate of " + str(bestTurnRateAns) + " degrees per second.")
-
+    print("That the " + aircraft.modelName + " has a substained turn rate of " + str(instantTurnRateAns) + " degrees per second, and substained turn radius of " + str(instantTurnradiusAns) + " meters, which is lower than its best turn rate.")
+    print("Where its best susbtained turn rate occurs at " + str(bestTurnSpeedAns) + " m/s. Giving it a turn radius of " + str(bestTurnRadiusAns) + " meters, at a turn rate of " + str(bestTurnRateAns) + " degrees per second.")
+    exit()
 
 if instantTurnThrustRequiredAns == (aircraft.thrust + aircraft.afterburner): 
     print("That the " + aircraft.modelName + " has a substained turn rate of " + str(instantTurnRateAns) + " degrees per second, and substained turn radius of " + str(instantTurnradiusAns) + " meters.")
+    exit()
